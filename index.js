126121145463ba888a544f2069a5ae08429e8ecb
Tom Varughese <stv1995@gmail.com>
2019-03-22 08:08:38 -0400

    fixed date range printing

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 //startDate
 const startEl = document.getElementById('start');
 
-
 //endDate
 const endEl = document.getElementById('end');
 
@@ -13,11 +12,18 @@ document.getElementById('submit').addEventListener('click', e => {
   //^sample output: {startValue: "2019-03-12", endValue: "2019-03-19"}
   var dates = getDates(new Date(startValue.substring(0, 4), startValue.substring(5, 7)-1, startValue.substring(8, 10)), new Date(endValue.substring(0, 4), endValue.substring(5, 7)-1, endValue.substring(8, 10)));
 
+  var listOfDates = "";
+
+  //for each item in dates array, print the date
   dates.forEach(function (date) {
-    console.log(date); // prints out the individual dates
+    listOfDates = listOfDates + date + "\n";
+
+    console.log(date);
+    
+    console.log(scheduledDates);
  });
 
-
+document.getElementById("scheduledDates").innerHTML = listOfDates;
 })
 
 console.log('memes loaded')
